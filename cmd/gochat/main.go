a52c63f2a2080f5f435e666acf0c02ac437534b1
chanma <yunosuke.homma@icloud.com>
2024-06-27 02:56:00 +0900

    [update]chat

--- cmd/gochat/main.go
+++ cmd/gochat/main.go
@@ -1,35 +1,73 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
 )
+
+var clients = make(map[*websocket.Conn]bool) // 接続されているクライアント
+var broadcast = make(chan Message)           // ブロードキャストメッセージ
+
+type Message struct {
+	Username string `json:"username"`
+	Message  string `json:"message"`
+}
 
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func handleConnections(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
 		return
 	}
 	defer conn.Close()
+	clients[conn] = true
+	for {
+		var message Message
+		// 新しいメッセージをJSONとして読み込み、Message構造体にマッピング
+		err := conn.ReadJSON(&message)
+		if err != nil {
+			log.Printf("error: %v", err)
+			delete(clients, conn)
+			break
+		}
+		// 受け取ったメッセージをbroadcastチャネルに送る
+		broadcast <- message
+	}
 }
 
-func Hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello")
+func handleMessages() {
+	for {
+		// メッセージを受信
+		msg := <-broadcast
+		// クライアントにメッセージを送信
+		for client := range clients {
+			err := client.WriteJSON(msg)
+			if err != nil {
+				log.Printf("error: %v", err)
+				client.Close()
+				delete(clients, client)
+			}
+		}
+	}
 }
 
 func main() {
 	// ファイルを指定する
 	fs := http.FileServer(http.Dir("web"))
-	fmt.Print("Hello")
-	http.Handle("/", fs)
-	//http.HandleFunc("/", Hello)
-	http.ListenAndServe(":9090", nil)
+	http.Handle("/template", fs)
+
+	http.HandleFunc("/ws", handleConnections)
+	go handleMessages()
+
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+		return
+	}
 }
