f004649011614924b2d0390241593c39ee3e2a59
chanma <yunosuke.homma@icloud.com>
2024-06-27 01:55:09 +0900

    [update]ファイルを返すように

--- cmd/gochat/main.go
+++ cmd/gochat/main.go
@@ -2,15 +2,34 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/websocket"
+	"log"
 	"net/http"
 )
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer conn.Close()
+}
 
 func Hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello")
 }
 
 func main() {
+	// ファイルを指定する
+	fs := http.FileServer(http.Dir("web"))
 	fmt.Print("Hello")
-	http.HandleFunc("/", Hello)
-	http.ListenAndServe(":8080", nil)
+	http.Handle("/", fs)
+	//http.HandleFunc("/", Hello)
+	http.ListenAndServe(":9090", nil)
 }
