e39fe40b2977fde43b5a7a52f4c0e8a05bd38779
chanma <yunosuke.homma@icloud.com>
2024-06-28 03:49:01 +0900

    [update]AuthCodeURLでstateを使うためにrandomな文字列を作成する関数を作った

---
+++ pkg/random.go
@@ -0,0 +1,16 @@
+package pkg
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+)
+
+// ランダムに文字列を生成する
+func GenerateRandomString() string {
+	b := make([]byte, 32)
+	if _, err := rand.Read(b); err != nil {
+		return ""
+	}
+	// base64 Encoding
+	return base64.URLEncoding.EncodeToString(b)
+}
