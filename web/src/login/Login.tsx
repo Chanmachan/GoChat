b81e85420d36661e6580d16920c20a762c0eba57
chanma <yunosuke.homma@icloud.com>
2024-06-29 21:31:04 +0900

    [update]loginするときのapiにアクセス、unauthorizeなら認証するように修正

--- web/src/login/Login.tsx
+++ web/src/login/Login.tsx
@@ -2,12 +2,35 @@ import React from 'react';
 
 const Login = () => {
   const handleLogin = async () => {
+    // 先にユーザー情報をチェック
+    fetch('http://localhost:9090/api/login', {
+      credentials: 'include'
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Unauthorized');
+        }
+      })
+      .then(data => {
+        console.log('Already logged in, redirecting:', data);
+        // 既にログインしていれば、部屋選択ページなどへリダイレクトする
+        window.location.href = '/room-selection';
+      })
+      .catch(error => {
+        console.log('Need to authenticate:', error);
+        // 認証されていない場合、OAuthを開始
+        authenticate();
+      });
+  };
+
+  const authenticate = () => {
     fetch('http://localhost:9090/auth/', {
       credentials: 'include'
     })
       .then(response => response.json())
       .then(data => {
-        console.log(data.url);
         window.location.href = data.url;  // URLからのリダイレクト
       })
       .catch(error => {
