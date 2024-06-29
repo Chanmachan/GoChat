66e6f7ffa8170820520f7c0bdb37cf72376c1c65
chanma <yunosuke.homma@icloud.com>
2024-06-29 14:49:36 +0900

    [update]エラーの出力

--- internal/api/userinfo.go
+++ internal/api/userinfo.go
@@ -3,6 +3,7 @@ package api
 import (
 	"encoding/json"
 	"github.com/Chanmachan/GoChat/pkg/auth"
+	"log"
 	"net/http"
 )
 
@@ -11,16 +12,19 @@ func UserInfoHandler(w http.ResponseWriter, r *http.Request) {
 	session, err := auth.GetStore().Get(r, "auth-session")
 	if err != nil {
 		http.Error(w, "Failed get session error: "+err.Error(), http.StatusInternalServerError)
+		log.Fatal("Failed get session")
 		return
 	}
 	userinfo, ok := session.Values["userInfo"].(auth.UserInfo)
 	if !ok || userinfo.Name == "" {
 		http.Error(w, "No user data available", http.StatusNotFound)
+		log.Fatal("No user data available")
 		return
 	}
 	respData, err := json.Marshal(userinfo)
 	if err != nil {
 		http.Error(w, "Failed marshal user data: "+err.Error(), http.StatusInternalServerError)
+		log.Fatal("Failed marshal user data")
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
