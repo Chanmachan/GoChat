729977712408f05cefd31d0e3a69f8fc09c66655
chanma <yunosuke.homma@icloud.com>
2024-06-30 06:17:49 +0900

    [update]google認証した後はroomselectionへ遷移するように

--- web/src/login/LoginState.tsx
+++ web/src/login/LoginState.tsx
@@ -1,33 +1,65 @@
-import {useLocation, useNavigate} from 'react-router-dom';
-import {useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { Box, CircularProgress, Text, Container, VStack } from '@chakra-ui/react';
+import {useUser} from '../contexts/UserContexts';
 
 const LoginState = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const queryParams = new URLSearchParams(location.search);
   const status = queryParams.get('status');
+  const [countdown, setCountdown] = useState(2);
+  const { setUserInfo } = useUser();
 
-  // 画面がロードされた後に実行される関数をセット
   useEffect(() => {
-    if (status === 'success') {
-      setTimeout(() => {
+    const timer = setInterval(() => {
+      setCountdown(prev => prev - 1);
+    }, 1000);
+
+    const timeout = setTimeout(() => {
+      if (status === 'success') {
+        fetch('http://localhost:9090/api/login', {
+          credentials: 'include'
+        })
+          .then(response => {
+            if (response.ok) {
+              return response.json();
+            } else {
+              throw new Error('Unauthorized');
+            }
+          })
+          .then(data => {
+            console.log('Already logged in, redirecting:', data);
+            // ユーザー情報をcontextに保存
+            setUserInfo(data);
+            // 既にログインしていれば、部屋選択ページなどへリダイレクトする
+            navigate('/room-selection');
+          })
+          .catch(error => {
+            console.log('Unauthorized: ', error);
+          });
         navigate('/room-selection');
-      }, 2000);
-    } else if (status === 'fail') {
-      setTimeout(() => {
+      } else {
         navigate('/login');
-      }, 2000);
-    }
+      }
+    }, 2000);
+
+    return () => {
+      clearTimeout(timeout);
+      clearInterval(timer);
+    };
   }, [status, navigate]);
 
   return (
-    <div>
-      {status === 'success' ? (
-        <h1>Login Successful!</h1>
-      ) : (
-        <h1>Login Failed</h1>
-      )}
-    </div>
+    <Container centerContent p={4}>
+      <VStack spacing={4} align="center" justify="center" minH="100vh">
+        <CircularProgress isIndeterminate color={status === 'success' ? 'green.300' : 'red.400'} thickness="12px" />
+        <Box p={4} borderWidth="1px" borderRadius="lg" bg={status === 'success' ? 'green.100' : 'red.100'}>
+          <Text fontSize="2xl" fontWeight="bold">{status === 'success' ? 'Authorization Successful!' : 'Authorization Failed'}</Text>
+          <Text fontSize="md">You will be redirected in {countdown} seconds...</Text>
+        </Box>
+      </VStack>
+    </Container>
   );
 };
 
