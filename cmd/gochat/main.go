21a730aff0580a14fbd92b1eb0939f59b0f19ef5
chanma <yunosuke.homma@icloud.com>
2024-06-29 17:31:52 +0900

    [update]apiにアクセスできるように

--- cmd/gochat/main.go
+++ cmd/gochat/main.go
@@ -4,7 +4,9 @@ import (
 	"github.com/Chanmachan/GoChat/internal/api"
 	"github.com/Chanmachan/GoChat/internal/pkg"
 	"github.com/Chanmachan/GoChat/pkg/auth"
+	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
+	"github.com/rs/cors"
 	"log"
 	"net/http"
 )
@@ -13,6 +15,13 @@ func main() {
 	if godotenv.Load() != nil {
 		log.Fatal("Failed to load .env file")
 	}
+	router := mux.NewRouter()
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000"}, // Reactアプリのオリジンを許可
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization"},
+		AllowCredentials: true, // 認証情報を許可
+	})
 	// auth
 	auth.SetUp()
 	http.HandleFunc("/auth/", auth.LoginHandler)
@@ -21,11 +30,16 @@ func main() {
 	fs := http.FileServer(http.Dir("web/static"))
 	http.Handle("/", fs)
 	// api
-	http.HandleFunc("/api/userinfo", api.UserInfoHandler)
+	// ハンドラーの設定
+	router.HandleFunc("/api/userinfo", api.UserInfoHandler).Methods("GET")
+	router.HandleFunc("/api/user", api.GetUserHandler).Methods("GET")
+
+	// CORSミドルウェアを使用
+	handler := c.Handler(router)
 	// クライアントからのWebSocketの接続を処理
 	http.HandleFunc("/ws", pkg.HandleConnections)
 
-	err := http.ListenAndServe(":9090", nil)
+	err := http.ListenAndServe(":9090", handler)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 		return
